59f2cdb0b8dcd21f1e176c0e2057b169f8cc28bf
Parviz Rahimli <parviz.rahimli@kibrit.tech>
2024-12-09 17:53:51 +0400

    refactoring

--- src/main/java/org/rahimliparviz/linkup/entity/User.java
+++ src/main/java/org/rahimliparviz/linkup/entity/User.java
@@ -1,13 +1,11 @@
 package org.rahimliparviz.linkup.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "Users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
